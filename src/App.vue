5821bf4c178df60af03174ff6a0ce82f090d20be
CristianN01 <cristiannechifor71@gmail.com>
2024-06-12 16:00:14 +0200

    creato scaffolding vue e vite, e scomposto il problema nel file redme

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+  data() {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<template>
+
+</template>
+
+<style lang="scss">
+@use './styles/general.scss';
+@use './styles/mixins' as *;
+@use './styles/variables' as *;
+
+</style>
